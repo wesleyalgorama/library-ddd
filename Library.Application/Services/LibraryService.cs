b80a4c6e8d234b2a2b718ff9ee3a57f56a46987d
wesleyaglorama <wesley@aglorama.com.br>
2025-01-03 15:16:53 -0300

    feat: add LibraryService

---
+++ Library.Application/Services/LibraryService.cs
@@ -0,0 +1,47 @@
+﻿namespace Library.Application.Services;
+
+using Library.Domain.Entities;
+using Library.Domain.Interfaces;
+
+public class LibraryService
+{
+    private readonly IBookRepository _bookRepository;
+
+    public LibraryService(IBookRepository bookRepository)
+    {
+        _bookRepository = bookRepository;
+    }
+
+    public IEnumerable<Book> GetAll()
+    {
+        return _bookRepository.GetAll();
+    }
+
+    public Book Get(long id)
+    {
+        var book = _bookRepository.Get(id);
+
+        if (book == null) throw new ArgumentException("Book not found.");
+
+        return book;
+    }
+
+    public void Add(Book book)
+    {
+        _bookRepository.Add(book);
+    }
+
+    public void Borrow(long id)
+    {
+        var book = Get(id);
+
+        book.Borrow();
+    }
+
+    public void Return(long id)
+    {
+        var book = Get(id);
+
+        book.Return();
+    }
+}
