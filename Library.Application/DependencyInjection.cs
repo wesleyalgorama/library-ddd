f4f1ce81fba49ab74927f2f6401e9b37e18959f3
wesleyaglorama <wesley@aglorama.com.br>
2025-01-03 15:17:24 -0300

    feat: add DependencyInjection

---
+++ Library.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿namespace Library.Application;
+
+using Library.Application.Services;
+using Library.Domain.Interfaces;
+using Library.Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddScoped<LibraryService>();
+        services.AddSingleton<IBookRepository, BookRepository>();
+        return services;
+    }
+}
